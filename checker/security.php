79cf35f89ac4ecaae3d6ee6da7689f0ae77ec2c7
Tongliang Li <tonyliangli@163.com>
2020-08-11 23:10:18 +0800

    demo v2 based on cesium first commit

--- checker/security.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    function cleanStr($content){
-        $content = str_replace(array ('"', "\\", "'", "/", "..", "../", "./", "//","<",">","|","||","*","(",")","^","!","&","&&","=" ), '', $content);
-        $content = str_replace(array ("script", "*","{","}" ), '', $content);
-        return $content;
-    }
-
-
-?>
--- three.js-r88/utils/build/externs.js
+++
@@ -1,7 +0,0 @@
-var THREE;
-var define;
-var module;
-var exports;
-var performance;
-var ImageBitmap;
-var WebGL2RenderingContext;
